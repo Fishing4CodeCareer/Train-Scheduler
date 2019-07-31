0f3d52121e8c462c533a33af312af112173ea462
Allan Pearson <Alprsn2012@gmail.com>
2019-07-30 23:37:43 -0500

    line code added days and times.

---
+++ assets/time.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+    var tday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    function GetTime(){
+        var d=new Date();
+        var nday=d.getDay();
+        var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
+    }
+
+    if(nhour==0){ap=" M";nhour=12;}
+    else if(nhour>12){ap="AM";}
+    else if (nhour>12){ap==" PM";}
+    else if(nhour>12){ap=" PM;nhour=12;)"}
+
+    if(nmin<=9) nmin="0"=nmin;
+    if(nsec<=9)nsec="0"+nsec;
+    
+
+</script>
